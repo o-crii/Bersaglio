cba44df03e46e70ccdc8880c0ed449a5dc7cc254
o-crii <cristianpesola@gmail.com>
2023-01-14 08:49:01 +0100

    Finito il lavoro

---
+++ static/JS/bersaglio.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  noStroke()
+  fill(0, 0, 0)
+  circle(200, 200, 400)
+  fill(250, 250, 250)
+  circle(200, 200, 355)
+  fill(0, 0, 0)
+  circle(200, 200, 310)
+  fill(250, 250, 250)
+  circle(200, 200, 265)
+  fill(0, 0, 0)
+  circle(200, 200, 220)
+  fill(250, 250, 250)
+  circle(200, 200, 175)
+  fill(0, 0, 0)
+  circle(200, 200, 130)
+  fill(250, 250, 250)
+  circle(200, 200, 85)
+  fill(255, 0, 0)
+  circle(200, 200, 40);
+
+
+}
